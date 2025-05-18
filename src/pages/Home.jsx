acf6a7b9cd2b1265df7972aaf4d6ba9e07d38e26
Ross Taylor-Turner <ross.turner001@googlemail.com>
2025-05-18 11:06:05 +0100

    Update homepage text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,14 @@ export default function Home() {
 
                         <Text size="lg" ta="center" maw={700}>
                             This site is my journal of studies, sketches and finished illustrations,
-                            charting progress from absolute beginner (Sept 2024) to today.
+                            charting progress from absolute beginner (September 2024) to today.
                         </Text>
-
                         <Text size="lg" ta="center" maw={700}>
-                            Click any thumbnail to dive into process shots, notes and timelapses.
+                            Though I don't get much time to draw and don't produce much each month,
+                            charting my progress here is a great motivator to keep adding to it!
+                        </Text>
+                        <Text size="lg" ta="center" maw={700}>
+                            Click any thumbnail to dive into process shots, notes, sometimes a timelapse and even the Clip Studio Paint file!
                         </Text>
                     </Stack>
                 </Container>
